8f96dbef497c19aaf2d12ac7280e8e4a20b78cd1
Joseph VanTine <joseph@dealersleague.com>
2020-11-30 14:04:52 +0000

    listing view

--- templates/content-archive-listing.php
+++ templates/content-archive-listing.php
@@ -31,15 +31,7 @@ if ( $layout_type == 'list' ) {
 ?>
 
 <div class="items grid-xl-4-items grid-lg-4-items grid-md-4-items list">
-    <div class="item">
-        <div class="ribbon-featured">
-            <div class="ribbon-start"></div>
-            <div class="ribbon-content">Featured</div>
-            <div class="ribbon-end">
-                <figure class="ribbon-shadow"></figure>
-            </div>
-        </div>
-        <!--end ribbon-->
+    <div class="item"> 
         <div class="wrapper">
             <div class="image">
                 <h3>
@@ -48,29 +40,36 @@ if ( $layout_type == 'list' ) {
                     <span class="tag">Offer</span>
                 </h3>
                 <a href="single-listing-1.html" class="image-wrapper background-image"
-                   style="background-image: url(&quot;file:///Users/walterbarcelos/Documents/DEALERS%20LEAGUE/WORDPRESS%20PLUGIN/elements-craigs-directory-listing-template-J2GNYE-nnAKn1Iy-02-13/html/assets/img/image-01.jpg&quot;);">
-                    <img src="assets/img/image-01.jpg" alt="">
+                   style="background-image: url('https://i.insider.com/5b59df8d1982d835008b460a?width=1136&format=jpeg');">
+                    <img src="https://i.insider.com/5b59df8d1982d835008b460a?width=1136&format=jpeg" alt="">
                 </a>
             </div>
             <!--end image-->
-            <h4 class="location">
-                <a href="#">Manhattan, NY</a>
-            </h4>
+             
             <div class="price">$80</div>
-            <div class="meta">
-                <figure>
-                    <i class="fa fa-calendar-o"></i>02.05.2017
-                </figure>
-                <figure>
-                    <a href="#">
-                        <i class="fa fa-user"></i>Jane Doe
-                    </a>
-                </figure>
+            <div class="meta"> 
+                    £12,000.00 
             </div>
             <!--end meta-->
             <div class="description">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis lobortis</p>
             </div>
+            <div class="additional-info">
+                                    <ul>
+                                        <li>
+                                            <figure>Brand</figure>
+                                            <aside>Jeep</aside>
+                                        </li>
+                                        <li>
+                                            <figure>Engine</figure>
+                                            <aside>Diesel</aside>
+                                        </li>
+                                        <li>
+                                            <figure>Mileage</figure>
+                                            <aside>28,630</aside>
+                                        </li>
+                                    </ul>
+                                </div>
             <!--end description-->
             <a href="single-listing-1.html" class="detail text-caps underline">Detail</a>
         </div>
