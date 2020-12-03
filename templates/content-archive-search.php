f4073a2fd3dc0fd201ffa5f4747e1535fd3863b7
walterbarcelosDL <Waltinho55!>
2020-12-03 09:40:10 +0100

    search form

--- templates/content-archive-search.php
+++ templates/content-archive-search.php
@@ -6,9 +6,9 @@
             <div class="form-row">
                 <div class="col-md-3 col-sm-3">
                     <div class="form-group">
-                        <label for="manufacturer" class="col-form-label">Manufacturer</label>
+                        <label for="manufacturer" class="col-form-label"><?php _e( 'Manufacturer', 'dlmarine' ); ?></label>
                         <select name="manufacturer" id="manufacturer" data-placeholder="Select Manufacturer">
-                            <option value="">Select Manufacturer</option>
+                            <option value=""><?php _e( 'Select Manufacturer', 'dlmarine' ); ?></option>
                             <option value="Integrity">Integrity</option>
                             <option value="Yamaha">Yamaha</option>
                             <option value="Bayliner">Bayliner</option>
@@ -19,9 +19,9 @@
                 <!--end col-md-3-->
                 <div class="col-md-3 col-sm-3">
                     <div class="form-group">
-                        <label for="category" class="col-form-label">Category</label>
+                        <label for="category" class="col-form-label"><?php _e( 'Category', 'dlmarine' ); ?></label>
                         <select name="category" id="category" data-placeholder="Select Category">
-                            <option value="">Select Category</option>
+                            <option value=""><?php _e( 'Select Category', 'dlmarine' ); ?></option>
                             <option value="motorboat">Motorboats</option>
                             <option value="sailboat">Sailboats</option>
                         </select>
@@ -30,14 +30,14 @@
                 </div>
                 <div class="col-md-3 col-sm-3">
                     <div class="form-group">
-                        <label for="price" class="col-form-label">Price</label>
+                        <label for="price" class="col-form-label"><?php _e( 'Price', 'dlmarine' ); ?></label>
                         <select name="price" id="price" data-placeholder="Select price">
-                            <option value="">Select Price</option>
-                            <option value="1">All Prices</option>
-                            <option value="0-19999">0 - 19,999</option>
-                            <option value="20000-49999">20,000 - 49,999</option>
-                            <option value="50000-99999">50,000 - 99,999</option>
-                            <option value="100000"><?php __( '100,000 and above', 'dlmarine' ); ?></option>
+                            <option value=""><?php _e( 'Select Price', 'dlmarine' ); ?></option>
+                            <option <?php echo (isset($search_price) && $search_price == 'all' ? 'selected="selected"' : '') ?> value="all"><?php _e( 'All Prices', 'dlmarine' ); ?></option>
+                            <option <?php echo (isset($search_price) && $search_price == '0-19999' ? 'selected="selected"' : '') ?> value="0-19999">0 - 19,999</option>
+                            <option <?php echo (isset($search_price) && $search_price == '20000-49999' ? 'selected="selected"' : '') ?> value="20000-49999">20,000 - 49,999</option>
+                            <option <?php echo (isset($search_price) && $search_price == '50000-99999' ? 'selected="selected"' : '') ?> value="50000-99999">50,000 - 99,999</option>
+                            <option <?php echo (isset($search_price) && $search_price == '100000' ? 'selected="selected"' : '') ?> value="100000"><?php _e( '100,000 and above', 'dlmarine' ); ?></option>
                         </select>
                     </div>
                     <!--end form-group-->
@@ -52,7 +52,7 @@
                 </div>
                 end col-md-3-->
                 <div class="col-md-3 col-sm-3">
-                    <button type="submit" class="btn btn-primary width-100">Search</button>
+                    <button type="submit" class="btn btn-primary width-100"><?php _e( 'Search', 'dlmarine' ); ?></button>
                 </div>
                 <!--end col-md-3-->
             </div>
@@ -61,36 +61,39 @@
         <!--end main-search-form-->
         <!--Alternative Form-->
         <div class="alternative-search-form">
-            <a href="#collapseAlternativeSearchForm" class="icon openAlternativeSearchForm" data-toggle="collapse"  aria-expanded="false" aria-controls="collapseAlternativeSearchForm"><i class="fa fa-plus"></i>More Options</a>
-            <div class="collapse" id="collapseAlternativeSearchForm">
+            <a href="#collapseAlternativeSearchForm" class="icon openAlternativeSearchForm" data-toggle="collapse"  aria-expanded="false" aria-controls="collapseAlternativeSearchForm"><i class="fa fa-plus"></i><?php _e( 'More Options', 'dlmarine' ); ?></a>
+            <?php
+            $show_collapse = isset($search_age)||isset($search_fuel)||isset($search_country)||isset($search_colour) ? 'show' : '' ;
+            ?>
+            <div class="collapse <?php echo $show_collapse; ?>" id="collapseAlternativeSearchForm">
                 <div class="wrapper">
                     <div class="form-row">
                         <div class="col-xl-3 col-lg-12 col-md-12 col-sm-12">
                               
                             <select name="age" id="age" data-placeholder="age" >
-                                <option value="">Age</option>
-                                <option value="1">New</option>
-                                <option value="2010-2020">2020 - 2010</option>
-                                <option value="2000-2009">2009 - 2000</option>
-                                <option value="1999"><?php __('1999 and older', 'dlmarine'); ?></option>
+                                <option value=""><?php _e('Age', 'dlmarine'); ?></option>
+                                <option <?php echo (isset($search_age) && $search_age == 'new' ? 'selected="selected"' : '') ?> value="new"><?php _e( 'New', 'dlmarine'); ?></option>
+                                <option <?php echo (isset($search_age) && $search_age == '2010-2020' ? 'selected="selected"' : '') ?> value="2010-2020">2020 - 2010</option>
+                                <option <?php echo (isset($search_age) && $search_age == '2000-2009' ? 'selected="selected"' : '') ?> value="2000-2009">2009 - 2000</option>
+                                <option <?php echo (isset($search_age) && $search_age == '1999' ? 'selected="selected"' : '') ?> value="1999"><?php __('1999 and older', 'dlmarine'); ?></option>
                             </select> 
 
                         </div>
                         <div class="col-xl-3 col-lg-12 col-md-12 col-sm-12">
                               
                             <select name="fuel" id="fuel" data-placeholder="fuel" >
-                                <option value="">Fuel Type</option>
-                                <option value="gasoline">Gasoline</option>
-                                <option value="diesel">Diesel</option>
-                                <option value="other">Other</option>
+                                <option value=""><?php _e( 'Fuel Type', 'dlmarine' ); ?></option>
+                                <option <?php echo (isset($search_fuel) && $search_fuel == 'gasoline' ? 'selected="selected"' : '') ?> value="gasoline"><?php _e( 'Gasoline', 'dlmarine' ); ?></option>
+                                <option <?php echo (isset($search_fuel) && $search_fuel == 'diesel' ? 'selected="selected"' : '') ?> value="diesel"><?php _e('Diesel', 'dlmarine' ); ?></option>
+                                <option <?php echo (isset($search_fuel) && $search_fuel == 'other' ? 'selected="selected"' : '') ?> value="other"><?php _e('Other', 'dlmarine' ); ?></option>
                             </select>
 
                         </div>
                         <div class="col-xl-3 col-lg-12 col-md-12 col-sm-12">
                               
                             <select name="country" id="country" data-placeholder="country" >
-                                <option value="">Country </option>
-                                <option value="1">All Countries</option>
+                                <option value=""><?php _e('Country', 'dlmarine'); ?></option>
+                                <option <?php echo (isset($search_country) && $search_country == 'all' ? 'selected="selected"' : '') ?> value="all"><?php _e( 'All Countries', 'dlmarine' ); ?></option>
                                 <option value="UK">United Kingdom</option>
                                 <option value="DE">Germany</option>
                             </select>
@@ -99,8 +102,8 @@
                         <div class="col-xl-3 col-lg-12 col-md-12 col-sm-12">
                               
                             <select name="colour" id="colour" data-placeholder="colour" >
-                                <option value="">Colour</option>
-                                <option value="1">All Colours</option>
+                                <option value=""><?php _e('Colour', 'dlmarine'); ?></option>
+                                <option <?php echo (isset($search_colour) && $search_colour == 'all' ? 'selected="selected"' : '') ?> value="all"><?php _e('All Colours', 'dlmarine'); ?></option>
                                 <option value="2">United Kingdom</option>
                                 <option value="3">Germany</option> 
                             </select>
