a58681a2006d868fedee48f8b5ba3d30120cb167
walterbarcelosDL <Waltinho55!>
2021-01-12 08:10:18 +0100

    price covered by boat name fix

--- templates/item-listing-list.php
+++ templates/item-listing-list.php
@@ -10,7 +10,7 @@
         <?php echo ($n_images ? '<div class="price"><i class="fa fa-image"></i> ' . $n_images . '</div>' : ''); ?>
         <?php echo ($n_videos ? '<div class="price" ' . ($n_images ? 'style="left: 7rem;"' : '') . '><i class="fa fa-video-camera"></i> ' . $n_videos . '</div>' : ''); ?>
              
-        <div class="meta">
+        <div class="meta d-none d-xl-block">
 	        <?php echo $currency . $price; ?>
         </div>
         <!--end meta-->
@@ -20,8 +20,15 @@
         <div class="additional-info">
             <h3>
                 <a href="#" class="tag category"><?php echo __( ucfirst( $category ) , 'dlmarine' ); ?></a>
-                <a href="<?php echo get_permalink( $listing->ID ); ?>" class="title"><?php echo $manufacturer . ' ' . $model; ?></a> 
+                <a href="<?php echo get_permalink( $listing->ID ); ?>" class="title"><?php echo $manufacturer . ' ' . $model; ?></a>
             </h3>
+
+            <ul class="d-block d-xl-none">
+                <li class="price-small">
+		        <?php echo $currency . $price; ?>
+                </li>
+            </ul>
+
             <ul>
                 <?php foreach ( $meta_field_list as $field_name => $field_value ) {
                     if ( ! empty( $field_value ) ) {
