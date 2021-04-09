efdee2c97e404ec9ce9ae27c5a1e02d0b890b1b6
Joseph VanTine <joseph@dealersleague.com>
2021-04-09 10:40:48 +0100

    .

--- dealersleague-marine.php
+++ dealersleague-marine.php
@@ -11,7 +11,8 @@ if ( ! defined( 'WPINC' ) ) {
  * Plugin URI:        https://dealearsleague.com
  * Description:       Manage, list, broker and sell your boats online, book a charter or advertise your repair shop. The largest and most complete WordPress boat plugin.
  * Version:           2.0.0
- * Author:            Walter Barcelos (walter@dealersleague.com), Joseph VanTine (joseph@dealersleague.com)
+ * Author:            josephbydesign, dealersleague
+ * Author URI:        https://profiles.wordpress.org/josephbydesign/, https://dealersleague.com
  * Text Domain:       dlmarine
  * Domain Path:       /languages
  */
